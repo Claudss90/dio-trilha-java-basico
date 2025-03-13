385ab9d722a42dc7cb6a1b269843e6ea4c0ad85d
Claudss90 <claudio-s.s@hotmail.com>
2025-03-12 21:15:21 -0300

    dio conta banco finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,39 @@
+
+import java.util.Scanner;
+
+
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //TODO: Conhecer e importa a classe scanner
+        //Exibir as mensagens para o nosso usuário
+        //Obter pela scanner os valores digitados no terminal
+        //exibir a mensagem conta criada
+
+        Scanner scan = new Scanner((System.in));
+
+        System.out.println("Digite seu Nome completo: ");
+        String nomeCliente = scan.next();
+
+        System.out.println("Digite o número da Agência: ");
+        String numeroAgencia = scan.next();
+
+        System.out.println("Digite o número da conta: ");
+        int numeroConta = scan.nextInt();
+
+        System.out.println("Qual o valor do primeiro depósito: ");
+        float saldo = scan.nextFloat();
+
+
+        System.out.println("------------------------------------------------------------------------------------------------------------------------");
+
+       
+        
+
+      System.out.println("Olá " +nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " +numeroAgencia + ", conta " +numeroConta +" e seu saldo R$:" +saldo + " já está disponível para saque.");
+
+      
+      System.out.println("------------------------------------------------------------------------------------------------------------------------");
+
     }
 }
